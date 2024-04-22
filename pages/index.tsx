5148badcf8812c5ca7cf4841d1b64c0672d09662
Eastyn <eastynwork@gmail.com>
2024-04-22 10:58:22 +0900

    add es-lint and .gitignore

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next/types";
+import React from "react";
+
+const Home: NextPage = () => {
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	);
+};
+
+export default Home;
