b6336cde580279b5124e27ed82682e68481b8948
Eastyn <eastynwork@gmail.com>
2024-05-03 17:48:38 +0900

    on the process of user profile update api but forgot to validate unique fields...

--- pages/api/users/profile/update.tsx
+++ pages/api/users/profile/update.tsx
@@ -4,7 +4,24 @@ import { withApiSession } from "../../../../lib/server/withSession";
 import withHandler from "../../../../lib/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-	console.log(req.body);
+	const { id, name, email, phone } = req.body;
+	// 해당 유저 id에 특정 필드가 추가되었을 때
+	// 유니크한 필드 (이메일, 폰)의 경우 겹치는 경우가 있는지 확인하고 업데이트 해야
+	const updateUser = await db.user.update({
+		where: {
+			id,
+		},
+		data: {
+			name,
+			email,
+			phone,
+		},
+	});
+
+	res.json({
+		ok: true,
+		updateUser,
+	});
 }
 
 export default withApiSession(withHandler({ methods: ["POST"], handler }));
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -8,7 +8,7 @@ import Button from "components/button";
 import useMutation from "lib/client/useMutation";
 
 interface IValidProfileForm {
-	id?: string;
+	id?: number;
 	name?: string;
 	email?: string;
 	phone?: number;
@@ -17,12 +17,20 @@ interface IValidProfileForm {
 export default function ProfileEdit() {
 	const { data } = useSWR("/api/users/profile");
 	console.log(data);
-	const { register, handleSubmit } = useForm();
-	const [updateProfile] = useMutation("/api/users/profile/update");
+	const { register, handleSubmit } = useForm({
+		defaultValues: {
+			id: data?.userProfile?.id,
+			name: data?.userProfile?.name,
+			email: data?.userProfile?.email ? data?.userProfile?.email : undefined,
+			phone: data?.userProfile?.phone ? data?.userProfile?.phone : undefined,
+		},
+	});
+	const [updateProfile, { data: updatedProfileData, loading }] = useMutation("/api/users/profile/update");
 	const [emailRequired, setEmailRequired] = useState(true);
 	const [phoneRequired, setPhoneRequired] = useState(false);
 
 	const onValid = (data: IValidProfileForm) => {
+		if (loading) return;
 		updateProfile(data);
 	};
 
@@ -33,6 +41,12 @@ export default function ProfileEdit() {
 		}
 	}, [data]);
 
+	useEffect(() => {
+		if (updatedProfileData?.ok) {
+			console.log(updatedProfileData);
+		}
+	}, [updatedProfileData]);
+
 	return (
 		<Layout title="Edit profile" canGoBack>
 			<Head>
@@ -41,17 +55,10 @@ export default function ProfileEdit() {
 
 			<form onSubmit={handleSubmit(onValid)} className="flex flex-col space-y-10">
 				<div className="flex flex-col space-y-4">
-					<Input register={register("userId", { required: true, disabled: true })} name="userId" label="User Id" type="text" value={data?.userProfile?.id} />
-					<Input register={register("name", { required: true })} name="name" label="Name" type="text" value={data?.userProfile?.name} />
-					<Input register={register("email", { required: emailRequired })} name="email" label="Email address" type="email" value={data?.userProfile?.email} />
-					<Input
-						register={register("phone", { required: phoneRequired })}
-						name="phone"
-						label="Phone number"
-						type="number"
-						kind="phone"
-						value={data?.userProfile?.phone ? data?.userProfile?.phone : undefined}
-					/>
+					<Input register={register("id", { required: true })} name="userId" label="User Id" type="number" value={data?.userProfile?.id} readOnly={true} />
+					<Input register={register("name", { required: true })} name="name" label="Name" type="text" />
+					<Input register={register("email", { required: emailRequired })} name="email" label="Email address" type="email" />
+					<Input register={register("phone", { required: phoneRequired })} name="phone" label="Phone number" type="number" kind="phone" />
 				</div>
 				<Button text="Update your profile" />
 			</form>
