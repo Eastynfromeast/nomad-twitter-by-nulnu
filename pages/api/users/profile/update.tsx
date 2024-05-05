2aad66b844dc328a641dc6ea47f650b3cb152034
Eastyn <eastynwork@gmail.com>
2024-05-05 21:51:42 +0900

    just another grass on Children's day

--- pages/api/users/profile/update.tsx
+++ pages/api/users/profile/update.tsx
@@ -7,7 +7,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { id, name, email, phone } = req.body;
 	// 해당 유저 id에 특정 필드가 추가되었을 때
 	// 유니크한 필드 (이메일, 폰)의 경우 겹치는 경우가 있는지 확인하고 업데이트 해야
-
 	let updateUser;
 	const thisUser = await db.user.findUnique({
 		where: {
@@ -15,6 +14,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 		},
 	});
 	if (!thisUser) return res.status(400).json({ ok: false, error: "사용자를 찾을 수 없습니다." });
+
 	if (thisUser?.email === email && thisUser?.phone === phone) {
 		updateUser = await db.user.update({
 			where: {
@@ -45,3 +45,12 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 }
 
 export default withApiSession(withHandler({ methods: ["POST"], handler }));
+
+/*
+    어떻게 검증하지?
+    일단 user id로 thisUser 찾아오고 
+    thisUser의 비어있는 필드를 찾고?
+    req.body에서 해당 필드가 있는지 확인하고? 
+    뭔가 스키마 필드가 잘못된 것 같다...
+    
+*/
